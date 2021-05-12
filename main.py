4439e1eae5bee52b4fc4b066f45d59a03c75bdac
xvenge00 <jawe1222@gmail.com>
2021-05-12 17:05:14 +0200

    write out to file

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import grpc
 import monitor_pb2
 import monitor_pb2_grpc
 import serial
+import sys
+
 
 def notify(stub):
     stub.Notify(monitor_pb2.google_dot_protobuf_dot_empty__pb2.Empty())
@@ -26,30 +28,39 @@ def contains_error_strings(line: str):
     return any(err in line.lower() for err in error_strings)
 
 
-def main():
-    # setup grpc client
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = monitor_pb2_grpc.EspMonitorStub(channel)
+def main(out):
+    # open file for output
+    with open(out, 'wb') as of:
+        # setup grpc client
+        channel = grpc.insecure_channel('localhost:50051')
+        stub = monitor_pb2_grpc.EspMonitorStub(channel)
 
-    # setup reading from serial port
-    ser = serial.Serial('/dev/ttyUSB0', baudrate=115200)
-    while True:
-        try:
-            ser_bytes = ser.readline()
-            if contains_error_strings(ser_bytes.decode('utf-8')):
-                try:
-                    print(ser_bytes)
-                    print("+========================================+",
-                            "|                                        |",
-                            "|              ERROR FOUND               |",
-                            "|                                        |",
-                            "+========================================+", sep='\n')
-                    notify(stub)
-                except:
-                    print("!!! detected FAILURE, but could not notify the monitor")
-        except UnicodeDecodeError:
-            pass
+        # setup reading from serial port
+        ser = serial.Serial('/dev/ttyUSB0', baudrate=115200)
+        while True:
+            try:
+                ser_bytes = ser.readline()
+                if contains_error_strings(ser_bytes.decode('utf-8')):
+                    try:
+                        print("+========================================+",
+                                "|                                        |",
+                                "|              ERROR FOUND               |",
+                                "|                                        |",
+                                "+========================================+", sep='\n')
+                        notify(stub)
+                    except:
+                        print("!!! detected FAILURE, but could not notify the monitor")
+                of.write(ser_bytes)
+                of.flush()
+            except UnicodeDecodeError:
+                pass
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        out = sys.argv[1]
+    else:
+        print("Invalid arguments")
+        exit(1)
+
+    main(out)
 
